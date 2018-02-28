38e7ecee729bd79b9582cafe133f4f0725731a84
ath <symphoneee@gmail.com>
2018-02-27 22:19:07 -0500

    updating repo

--- main.cpp
+++ main.cpp
@@ -5,8 +5,18 @@
 int main () {
 
     simon s1;    
-    s1.setHp(69);
-    std::cout << "Simon's current HP is " << s1.getHp() << std::endl;
+    simon s2;
+    simon s3;
+    simon s4;
 
-return 0;
+    s1.setHp(69);
+    std::cout << "Simon 1's current HP is " << s1.getHp() << std::endl;
+    
+    s2.setHp(70);
+    std::cout << "Simon 2's current HP is " << s2.getHp() << std::endl;
+    
+    s3.setHp(71);
+    std::cout << "Simon 3's current HP is " << s3.getHp() << std::endl;
+
+    return 0;
 };
